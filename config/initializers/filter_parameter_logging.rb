d12ff5b38ea4bc9ec51d507d530fef5f87a465d1
Corey Donohoe <atmos@atmos.org>
2014-03-13 00:59:05 -0700

    see if these guys cut it

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,1 +1,2 @@
-Rails.application.config.filter_parameters += [:password, :payload, :config]
+param_keys = [:description, :id, :name, :password, :payload, :repository, :sender, :sha, :state, :target_url]
+Rails.application.config.filter_parameters += param_keys
