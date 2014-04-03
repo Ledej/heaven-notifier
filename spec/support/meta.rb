858c8526135b7322157eea7630bc77ace2c227c4
Corey Donohoe <atmos@atmos.org>
2014-04-02 18:15:40 -0700

    different api for using oauth creds

--- spec/support/meta.rb
+++ spec/support/meta.rb
@@ -2,8 +2,8 @@ module Meta
   def stub_meta
     body = JSON.load(fixture_data("meta"))
 
-    stub_request(:get, "https://api.github.com/meta").
-      with(:headers => {'Accept'=>'application/vnd.github.beta+json', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Authorization'=>'token <unknown>', 'User-Agent'=>'Octokit Ruby Gem 2.5.1'}).
+    stub_request(:get, "https://api.github.com/meta?client_id=%3Cunknown-client-id%3E&client_secret=%3Cunknown-client-id%3E").
+      with(:headers => {'Accept'=>'application/vnd.github.beta+json', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Octokit Ruby Gem 2.5.1'}).
       to_return(:status => 200, :body => double("hooks" => body["hooks"]))
   end
 
